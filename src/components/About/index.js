1dcf0de51003d4b5f5f387437e2a58cc105f2890
lauracapella <laura@laguapacomunicacion.com>
2023-01-30 14:08:31 +0100

    about text page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -13,15 +13,9 @@ const About = () => {
                 />
             </h1>
             <p>
-            I am an enthusiastic and pixel-perfeccionist web developer,
-            I contribute from the front-end to the development of applications UX-oriented web and UI. I have solid
-            experience in web design, digital communication and
-            web layout. Throughout my career my interest in
-            programming has grown and recently I studied front-end development (Javascript, Vue and React).
-            I am looking for a position as a web layout artist and
-            developer of web applications where I can add value both for my programming knowledge as well as my experience
-            in web design and layout.
-            </p>
+            I am an enthusiastic and pixel-perfeccionist web developer, I contribute from the front-end to the development of applications UX-oriented web and UI.</p>
+            <p>I have solid experience in web design, digital communication and web layout. Throughout my career my interest in programming has grown and recently I studied front-end development (Javascript, Vue and React).</p>
+            <p>I am looking for a position as a web layout artist and developer of web applications where I can add value both for my programming knowledge as well as my experience in web design and layout.</p>
         </div>
     </div>
     )

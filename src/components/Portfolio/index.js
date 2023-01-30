38c07cf7c14d3966981068cebd9dff94075b4477
lauracapella <laura@laguapacomunicacion.com>
2023-01-30 22:35:47 +0100

    portfolio title

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Loader from 'react-loaders';
+import AnimatedLetters from '../AnimatedLetters';
 import './index.scss'
 
 const Portfolio = () =>{
+    const[letterClass, setLetterClass] = useState('text-animate');
     return (
+        <>
         <div className='container portfolio-page'>
-            <h1 className='page-title'>Works / Personal projects</h1>
+            <h1 className='page-title'>
+            <AnimatedLetters 
+                letterClass={letterClass}
+                strArray={'Portfolio'.split("")}
+                idx={15}
+            />
+            </h1>
 
         </div>
+        <Loader type='pacman'/>
+        </>
     )
 }
 

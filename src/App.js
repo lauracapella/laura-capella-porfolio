2b77f0ca0e353711db77b5506430cb1ecdf823de
lauracapella <laura@laguapacomunicacion.com>
2023-01-31 19:06:58 +0100

    contact page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Layout from './components/Layout';
 import Home from './components/Home'
 import Portfolio from './components/Portfolio'
 import About from './components/About'
+import Contact from './components/Contact';
 
 function App() {
   return (
@@ -13,6 +14,8 @@ function App() {
       <Route index element={<Home/>}/>
       <Route path="/about" element={<About/>}/>
       <Route path="/portfolio" element={<Portfolio/>}/>
+      <Route path="/contact" element={<Contact/>}/>
+
     </Route>
 
 
